cfadbac8030fcd7385297aa8a8f5ee5aab130d89
Llewellyn Falco <llewellyn.falco@gmail.com>
2020-08-28 03:42:09 +0000

    F add suffix
    Co-Authored-By: Maciek Kumorek <makumo@microsoft.com>
    Co-Authored-By: Henry Golding <13440904+hegold@users.noreply.github.com>
    Co-Authored-By: Kody Fintak <15849743+kodyfint@users.noreply.github.com>
    Co-Authored-By: Blake Lindsay <2676299+gotmonkey@users.noreply.github.com>

--- _03PassingObjects.cs
+++ _03PassingObjects.cs
@@ -16,26 +16,33 @@ namespace ManyOfOne
             Assert.AreEqual("*1", Format(1, new Options() { Prefix = "*"}));
         }
         
-        [Test, Ignore("Until ready")]
+        [Test]
         public void Test3AllowForCustomPrefixAndSuffix()
         {
             var expected = "#1)";
-            Assert.AreEqual(expected, Format(1 /*, ?*/));
+            Assert.AreEqual(expected, Format(1, new Options(){ Suffix = ")"}));
         }
 
         private string Format(int number, Options options = default)
         {
-            return options.Prefix + number ;
+            return options.Prefix + number + options.Suffix;
         }
 
         private struct Options
         {
             private string _prefix;
+            private string _suffix;
 
             public string Prefix
             {
                 get => _prefix ?? "#";
                 set => _prefix = value;
+            }
+
+            public string Suffix
+            {
+                get => _suffix ?? string.Empty;
+                set => _suffix = value;
             }
         }
     }
