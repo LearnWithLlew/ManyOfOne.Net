c3c8b09123ff4f8d44defbcf96005bdb8c916853
Llewellyn Falco <llewellyn.falco@gmail.com>
2020-08-28 04:01:43 +0000

    *** more scenarios
    Co-Authored-By: Maciek Kumorek <makumo@microsoft.com>
    Co-Authored-By: Henry Golding <13440904+hegold@users.noreply.github.com>
    Co-Authored-By: Kody Fintak <15849743+kodyfint@users.noreply.github.com>
    Co-Authored-By: Blake Lindsay <2676299+gotmonkey@users.noreply.github.com>

--- _04ApprovalTests.cs
+++ _04ApprovalTests.cs
@@ -6,13 +6,13 @@ using NUnit.Framework;
 
 namespace ManyOfOne
 {
-    [UseReporter(typeof(ClipboardReporter))]
+    [UseReporter(typeof(DiffReporter))]
     public class _04ApprovalTests
     {
         [Test]
         public void TestFizzBuzz()
         {
-            int[] numbers = new []{1};
+            int[] numbers = new []{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
             var result = FizzBuzz(numbers[0]);
             Approvals.VerifyAll("FizzBuzz", numbers, n => FizzBuzz(n));
         }
